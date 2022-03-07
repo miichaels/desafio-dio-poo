dd904043e304efee37442f98808dd748d3136030
miichaels <michael_araujo2012@hotmail.com>
2022-03-07 10:32:59 -0300

    Mentoria e Curso

---
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -0,0 +1,44 @@
+package br.com.dio.desafio.dominio;
+
+public class Curso {
+
+   private String titulo;
+   private String Descricao;
+   private int cargaHoraria;
+
+    public Curso() {
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return Descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        Descricao = descricao;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "titulo='" + titulo + '\'' +
+                ", Descricao='" + Descricao + '\'' +
+                ", cargaHoraria=" + cargaHoraria +
+                '}';
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+}
